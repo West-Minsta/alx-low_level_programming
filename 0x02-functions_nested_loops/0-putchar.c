97cff629f02b88a12a953b6ca8a9da602f6a291b
West Minsta <westminsta@gmail.com>
2022-03-15 17:15:57 +0300

    shell project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * printer - function name
+ * Description: prints _putchar when called
+ * Return: 0
+ */
+void printer(void)
+{
+printf("_putchar\n");
+return;
+}
+/**
+ * main - Entry block
+ * Description: prints _putchar on new line
+ * Return: 0
+ */
+int main(void)
+{
+printer();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This folder deals with nested loops and functions in c .
