517a9ee618154df50a94583567a9e385973feabd
West Minsta <westminsta@gmail.com>
2022-03-15 20:11:34 +0300

    shell project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *description :prints alphabets in lowercase
+ *main : prints alphabets 
+ *return 0:success
+ */
+int main(void)
+{
+    print_alphabet;
+    return (0);
+}
