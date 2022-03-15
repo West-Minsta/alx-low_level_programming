1965985b9b4e2ff1510203438f7f0ad4a37fca4c
West Minsta <westminsta@gmail.com>
2022-03-15 20:15:20 +0300

    shell project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,17 @@
  *main : prints alphabets 
  *return 0:success
  */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
 int main(void)
 {
     print_alphabet;
